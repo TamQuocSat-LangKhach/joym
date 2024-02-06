30bce0de4c9c4425e68c69de0889ad62079e072b
北冰洋T <13960651+arctic-ocean-t@user.noreply.gitee.com>
2024-02-06 10:32:07 +0000

    update joy_ex.lua. 增加了——界司马懿！
    Signed-off-by: 北冰洋T <13960651+arctic-ocean-t@user.noreply.gitee.com>

--- joy_ex.lua
+++ joy_ex.lua
@@ -417,4 +417,99 @@ Fk:loadTranslationTable{
   ["#joyex__qizhi-choose"] = "奇制：弃置一名角色一张牌，若为%arg，你摸一张牌，否则其摸一张牌",
 }
 
+
+local simayi = General(extension, "joyex__simayi", "wei", 3)
+local joyex__guicai = fk.CreateTriggerSkill{
+  name = "joyex__guicai",
+  anim_type = "control",
+  events = {fk.AskForRetrial},
+  can_trigger = function(self, event, target, player, data)
+    return player:hasSkill(self) and not player:isNude()
+  end,
+  on_cost = function(self, event, target, player, data)
+    local card = player.room:askForResponse(player, self.name, ".|.|.|hand,equip|.|", "#joyex__guicai-ask::" .. target.id .. ":" .. data.reason, true)
+    if card ~= nil then
+      self.cost_data = card
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    player.room:retrial(self.cost_data, player, data, self.name)
+    if not player.dead and self.cost_data.suit == Card.Club  then
+      player:drawCards(2, self.name)
+    end
+    if not player.dead and self.cost_data.suit == Card.Heart  then
+      player.room:recover({
+      who = player,
+      num = 1,
+      recoverBy = player,
+      skillName = self.name
+      })
+    end
+  end,
+}
+local joyex__fankui = fk.CreateTriggerSkill{
+  name = "joyex__fankui",
+  anim_type = "masochism",
+  events = {fk.Damaged},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and data.from and not data.from.dead and not data.from:isNude()
+  end,
+  on_trigger = function(self, event, target, player, data)
+    self.cancel_cost = false  
+    for i = 1, data.damage do
+      if self.cancel_cost then break end    
+      if data.from:isNude() then break end
+      self:doCost(event, target, player, data)
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room  
+    if room:askForSkillInvoke(player, self.name, data) then
+      return true
+    end
+    self.cancel_cost = true
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local from = data.from
+    if from and not from.dead then room:doIndicate(player.id, {from.id}) end
+    local judge = {
+      who = player,
+      reason = self.name,
+      pattern = ".",
+    }
+    room:judge(judge)
+    if judge.card.suit == Card.Heart and from and not from.dead then
+      local targets = table.map(table.filter(room.alive_players, function(p) return not p:isAllNude() end), Util.IdMapper)
+      if #targets == 0 then return end
+      local tos = room:askForChoosePlayers(player, targets, 1, 1, "#joyex__fankui-discard", self.name, false)
+      local to = room:getPlayerById(tos[1])
+      local card1 = room:askForCardChosen(player, to, "he", self.name)
+      room:obtainCard(player, card1, false, fk.ReasonPrey)
+    elseif judge.card.suit ~= Card.Heart and from and not from:isNude() then
+      local card2 = room:askForCardChosen(player, data.from, "he", self.name)
+      room:obtainCard(player, card2, false, fk.ReasonPrey)
+    end
+  end,
+}
+simayi:addSkill(joyex__guicai)
+simayi:addSkill(joyex__fankui)
+Fk:loadTranslationTable{
+  ["joyex__simayi"] = "界司马懿",
+  ["joyex__guicai"] = "鬼才",
+  [":joyex__guicai"] = "任何判定牌生效前，你可以打出一张牌代替之，若此牌为红桃，你回复1点体力，若此牌为梅花，你摸两张牌。",
+  ["joyex__fankui"] = "反馈",
+  [":joyex__fankui"] = "当你受到1点伤害后，你可以进行判定：红桃，你获得场上任意角色的一张牌：其他花色，你获得伤害来源一张牌。",
+
+  ["#joyex__guicai-ask"] = "鬼才：你可打出一张牌代替 %dest 的 %arg 判定;若为红桃，你回复1点体力，若为梅花，你摸两张牌。",
+  ["#joyex__fankui-discard"] = "反馈：选择任意一名角色获得其一张牌。",
+
+  ["$joyex__guicai1"] = "天命难违？哈哈哈哈哈……",
+  ["$joyex__guicai2"] = "才通天地，逆天改命！",
+  ["$joyex__fankui1"] = "哼，自作孽不可活！",
+  ["$joyex__fankui2"] = "哼，正中下怀！",
+  ["~joyex__simayi"] = "我的气数，就到这里了么？",
+}
+
 return extension
