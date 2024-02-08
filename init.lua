d1837ab8a7228b6b78c0b229b5504f9d08084b63
seven <786852516@qq.com>
2024-02-09 00:16:15 +0800

    严颜

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local joy_sp = require "packages/joym/joy_sp"
 local joy_nya = require "packages/joym/joy_nya"
 local joy_ex = require "packages/joym/joy_ex"
+local joy_shzl = require "packages/joym/joy_shzl"
 local joy_yj = require "packages/joym/joy_yj"
 local joy_re = require "packages/joym/joy_re"
 
@@ -10,6 +11,7 @@ return {
   joy_sp,
   joy_nya,
   joy_ex,
+  joy_shzl,
   joy_yj,
   joy_re,
 }
