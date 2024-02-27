ab6bf2252e065ff858f1b5aee5298de8dc870a49
北冰洋T <2513672919@qq.com>
2024-02-27 04:10:30 +0000

    sp庞统
    Signed-off-by: 北冰洋T <2513672919@qq.com>

--- joy_sp.lua
+++ joy_sp.lua
@@ -881,7 +881,101 @@ Fk:loadTranslationTable{
 
 
 
+local pangtong = General(extension, "joysp__pangtong", "wu", 3)
+local guolun = fk.CreateActiveSkill{
+  name = "joy__guolun",
+  anim_type = "control",
+  card_num = 0,
+  target_num = 1,
+  can_use = function(self, player)
+    return player:usedSkillTimes(self.name, Player.HistoryPhase) == 0
+  end,
+  card_filter = Util.FalseFunc,
+  target_filter = function(self, to_select, selected, selected_cards)
+    return #selected == 0 and to_select ~= Self.id and not Fk:currentRoom():getPlayerById(to_select):isKongcheng()
+  end,
+  on_use = function(self, room, effect)
+    local player = room:getPlayerById(effect.from)
+    local target = room:getPlayerById(effect.tos[1])
+    local id1 = room:askForCardChosen(player, target, "h", self.name)
+    target:showCards(id1)
+    if not target.dead and not player:isNude() then
+      local n1 = Fk:getCardById(id1).number
+      local card = room:askForCard(player, 1, 1, false, self.name, true, ".", "#joy__guolun-card:::"..tostring(n1))
+      if #card > 0 then
+        local id2 = card[1]
+        player:showCards(id2)
+        local n2 = Fk:getCardById(id2).number
+        if player.dead then return end
+        local move1 = {
+          from = effect.from,
+          ids = {id2},
+          to = effect.tos[1],
+          toArea = Card.PlayerHand,
+          moveReason = fk.ReasonJustMove,
+          proposer = effect.from,
+          skillName = self.name,
+        }
+        local move2 = {
+          from = effect.tos[1],
+          ids ={id1},
+          to = effect.from,
+          toArea = Card.PlayerHand,
+          moveReason = fk.ReasonJustMove,
+          proposer = effect.from,
+          skillName = self.name,
+        }
+        room:moveCards(move1, move2)
+        if n2 > n1 and not target.dead then
+          target:drawCards(1, self.name)
+          room:recover({
+            who = player,
+            num = 1,
+            recoverBy = player,
+            skillName = self.name,
+          })
+        elseif n1 > n2 and not player.dead then
+          player:drawCards(2, self.name)
+        end
+      end
+    end
+  end,
+}
+local songsang = fk.CreateTriggerSkill{
+  name = "joy__songsang",
+  anim_type = "support",
+  events = {fk.Death},
+  can_trigger = function(self, event, target, player, data)
+    return player:hasSkill(self) 
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    room:changeMaxHp(player, 1)
+    room:recover({
+      who = player,
+      num = 1,
+      recoverBy = player,
+      skillName = self.name,
+    })
+  end,
+}
+pangtong:addSkill(guolun)
+pangtong:addSkill(songsang)
+pangtong:addSkill("zhanji")
+Fk:loadTranslationTable{
+  ["joysp__pangtong"] = "庞统",
+  ["#joysp__pangtong"] = "南州士冠",
 
+  ["joy__guolun"] = "过论",
+  [":joy__guolun"] = "出牌阶段限一次，你可以展示一名其他角色的一张手牌，然后你可以展示一张手牌，交换这两张牌"..
+    "若其选择的点数小，其摸一张牌，你回复一点体力；"..
+    "若你选择的点数小，你摸两张牌",
+  ["joy__songsang"] = "送丧",
+  [":joy__songsang"] = "当其他角色死亡时，你可加1点体力上限并回复1点体力。",
+
+  ["#joy__guolun-card"] = "过论：你可以选择一张牌并交换双方的牌（对方点数为%arg）",
+
+}
 
 
 return extension
