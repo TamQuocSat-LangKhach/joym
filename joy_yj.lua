53ad5c7a24c376301963a6491a0f53456b736caa
北冰洋T <2513672919@qq.com>
2024-02-28 05:40:20 +0000

    界曹休
    Signed-off-by: 北冰洋T <2513672919@qq.com>

--- joy_yj.lua
+++ joy_yj.lua
@@ -478,4 +478,89 @@ Fk:loadTranslationTable{
 
 }
 
+local caoxiu = General(extension, "joy__caoxiu", "wei", 4)
+local joy__qingxi = fk.CreateTriggerSkill{
+  name = "joy__qingxi",
+  events = {fk.TargetSpecified},
+  anim_type = "offensive",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and (data.card.trueName == "slash" or data.card.trueName == "duel")
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    local n = 0
+    for _, p in ipairs(room.alive_players) do
+      if player:inMyAttackRange(p) then
+        n = n + 1
+      end
+    end
+    local max_num = #player:getEquipments(Card.SubtypeWeapon) > 0 and 4 or 2
+    n = math.min(n, max_num)
+    if player.room:askForSkillInvoke(player, self.name, data, "#joy__qingxi::" .. data.to..":"..n) then
+      self.cost_data = n
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local to = room:getPlayerById(data.to)
+    local num = self.cost_data
+    if #room:askForDiscard(to, num, num, false, self.name, true, ".", "#joy__qingxi-discard:::"..num) == num then
+      local weapon = player:getEquipments(Card.SubtypeWeapon)
+      if #weapon > 0 then
+        room:throwCard(weapon, self.name, player, to)
+      end
+    else
+      data.extra_data = data.extra_data or {}
+      data.extra_data.ty_ex__qingxi = data.to
+      local judge = {
+        who = player,
+        reason = self.name,
+        pattern = ".|.|club,spade,heart,diamond",
+      }
+      room:judge(judge)
+      if judge.card.color == Card.Red then
+        data.disresponsive = true
+      elseif judge.card.color == Card.Black and not player.dead then
+        player:drawCards(2,self.name)
+      end
+    end
+  end,
+}
+local joy__qingxi_delay = fk.CreateTriggerSkill{
+  name = "#joy__qingxi_delay",
+  events = {fk.DamageCaused},
+  mute = true,
+  can_trigger = function(self, event, target, player, data)
+    if target == player then
+      local e = player.room.logic:getCurrentEvent():findParent(GameEvent.CardEffect)
+      if e then
+        local use = e.data[1]
+        if use.extra_data and use.extra_data.ty_ex__qingxi == data.to.id then
+          return true
+        end
+      end
+    end
+  end,
+  on_cost = Util.TrueFunc,
+  on_use = function(self, event, target, player, data)
+    data.damage = data.damage + 1
+  end,
+}
+joy__qingxi:addRelatedSkill(joy__qingxi_delay)
+caoxiu:addSkill("qianju")
+caoxiu:addSkill(joy__qingxi)
+Fk:loadTranslationTable{
+  ["joy__caoxiu"] = "界曹休",
+  ["#joy__caoxiu"] = "千里骐骥",
+
+  ["joy__qingxi"] = "倾袭",
+  [":joy__qingxi"] = "当你使用【杀】或【决斗】指定一名角色为目标后，你可以令其选择一项："..
+  "1.弃置等同于你攻击范围内的角色数张手牌（至多为2，若你武器区里有武器牌则改为至多为4），然后弃置你装备区里的武器牌；"..
+  "2.令此牌对其造成的基础伤害值+1且你进行一次判定，若结果为红色，该角色不能响应此牌;若结果为黑色，你摸两张牌",
+  ["#joy__qingxi"] = "倾袭：可令 %dest 选一项：1.弃 %arg 张手牌并弃置你的武器；2.伤害+1且你判定，为红不能响应，为黑摸两张牌",
+  ["#joy__qingxi-discard"] = "倾袭：你需弃置 %arg 张手牌，否则伤害+1且其判定，结果为红你不能响应,结果为黑其摸两张牌",
+
+}
+
 return extension
